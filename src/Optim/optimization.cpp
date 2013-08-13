595f4ae885c346e28f679d40600d0687fa4bf40a
Marc Toussaint <marc.toussaint@fu-berlin.de>
2013-08-13 19:18:06 +0200

    PR2 grasps somehow funny ;-)

--- src/Optim/optimization.cpp
+++ src/Optim/optimization.cpp
@@ -580,7 +580,7 @@ uint optGaussNewton(arr& x, VectorFunction& f, OptOptions o, arr *addRegularizer
   if(o.fmin_return) *o.fmin_return=fx;
   if(o.verbose>0) fil.close();
 #ifndef MT_MSVC
-  if(o.verbose>1) gnuplot("plot 'z.gaussNewton' us 1:3 w l",NULL,true);
+  if(o.verbose>1) gnuplot("plot 'z.opt' us 1:3 w l",NULL,true);
 #endif
   return evals;
 }
