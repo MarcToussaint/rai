ebec3258360cdedf60304e3bd5c206c561a59946
Marc Toussaint <marc.toussaint@fu-berlin.de>
2013-11-13 17:15:49 +0100

    MDP revived

--- src/Core/util.cpp
+++ src/Core/util.cpp
@@ -1124,22 +1124,27 @@ std::string getcwd_string() {
 //
 
 #include "util_t.h"
-template void MT::getParameter(double&, const char*);
-template float MT::getParameter<float>(const char*);
-template double MT::getParameter<double>(const char*);
-template void MT::getParameter(uint&, const char*);
-template int MT::getParameter<int>(const char*);
 template void MT::getParameter(int&, const char*);
 template void MT::getParameter(int&, const char*, const int&);
+template void MT::getParameter(uint&, const char*);
+template void MT::getParameter(uint&, const char*, const uint&);
 template void MT::getParameter(bool&, const char*, const bool&);
-template int  MT::getParameter(const char*, const int&);
-template uint MT::getParameter(const char*, const uint&);
-template bool MT::getParameter(const char*, const bool&);
-template double MT::getParameter(const char*, const double&);
-template long MT::getParameter(const char*);
+template void MT::getParameter(double&, const char*);
+template void MT::getParameter(double&, const char*, const double&);
+template void MT::getParameter(MT::String&, const char*, const MT::String&);
+
+template int MT::getParameter<int>(const char*);
+template int  MT::getParameter<int>(const char*, const int&);
 template uint MT::getParameter(const char*);
-template MT::String MT::getParameter(const char*);
-template MT::String MT::getParameter(const char*, const MT::String&);
+template uint MT::getParameter<uint>(const char*, const uint&);
+template float MT::getParameter<float>(const char*);
+template double MT::getParameter<double>(const char*);
+template double MT::getParameter<double>(const char*, const double&);
+template bool MT::getParameter<bool>(const char*, const bool&);
+template long MT::getParameter<long>(const char*);
+template MT::String MT::getParameter<MT::String>(const char*);
+template MT::String MT::getParameter<MT::String>(const char*, const MT::String&);
+
 template bool MT::checkParameter<uint>(const char*);
 
 template void MT::Parameter<MT::String>::initialize();
