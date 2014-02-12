b01762da9916b088c1df869199d7a8dbe22260e2
Marc Toussaint <marc.toussaint@fu-berlin.de>
2014-02-12 13:05:20 +0100

    compile fixed after merge with rss14

--- src/Core/array.cpp
+++ src/Core/array.cpp
@@ -1609,6 +1609,7 @@ template MT::Array<arr>::~Array();
 
 #include "util_t.h"
 template MT::Array<double> MT::getParameter<MT::Array<double> >(char const*);
+template MT::Array<uint> MT::getParameter<MT::Array<uint> >(char const*);
 template bool MT::checkParameter<MT::Array<double> >(char const*);
 template void MT::getParameter(uintA&, const char*, const uintA&);
 
