910bf6b03de4b5af90ae71b9d90c4d3a9dbe0cb2
Ingo Lütkebohle <Ingo.Luetkebohle@ipvs.uni-stuttgart.de>
2014-02-13 14:05:56 +0100

    factor out rgb2yuv conversion

--- src/Perception/videoEncoderX264.cpp
+++ src/Perception/videoEncoderX264.cpp
@@ -1,59 +1,9 @@
 #include "videoEncoder.h"
+#include "colorspace.h"
 #include <Core/util.h>
 
 #include <opencv2/opencv.hpp>
 using namespace cv;
-
-namespace {
-#define PREVCOL_SHIFT   10
-#define PREVCOL_HALF    (1 << (PREVCOL_SHIFT - 1))
-#define PREVCOL_FIX(x)  ((int) ((x) * (1<<PREVCOL_SHIFT) + 0.5))
-
-/*
- * rgb to yuv conversion taken from icewing: http://icewing.sf.net/
- *
- * Copyright (C) 1999-2009
- * Frank Loemker, Applied Computer Science, Faculty of Technology,
- * Bielefeld University, Germany
- *
- * This file is part of iceWing, a graphical plugin shell.
- *
- * iceWing is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * iceWing is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- */
-
-
-/*********************************************************************
-  Perform RGB to YUV conversion with intervall reduction.
-*********************************************************************/
-static inline void rgbToYuvVis (const uint8_t rc, const uint8_t gc, const uint8_t bc,
-                                            uint8_t *yc, uint8_t *uc, uint8_t *vc)
-
-{
-    int y_shift = PREVCOL_FIX(0.299*219.0/255.0) * rc +
-            PREVCOL_FIX(0.587*219.0/255.0) * gc +
-            PREVCOL_FIX(0.114*219.0/255.0) * bc + PREVCOL_HALF;
-    int y = y_shift >> PREVCOL_SHIFT;
-
-    *uc = ((PREVCOL_FIX(0.564*224.0/255.0) * bc -
-            PREVCOL_FIX(0.564*224.0/219.0) * y + PREVCOL_HALF) >> PREVCOL_SHIFT) + 128;
-    *vc = ((PREVCOL_FIX(0.713*224.0/255.0) * rc -
-            PREVCOL_FIX(0.713*224.0/219.0) * y + PREVCOL_HALF) >> PREVCOL_SHIFT) + 128;
-    *yc = (y_shift + (16 << PREVCOL_SHIFT)) >> PREVCOL_SHIFT;
-}
-
-}
 
 #ifdef HAVE_X264
 
@@ -153,26 +103,12 @@ void sVideoEncoder_x264_simple::addFrame(const byteA& rgb){
   /* encode the image */
   //fflush(stdout);
   clock_gettime(CLOCK_REALTIME, &start_ts);
-  uint8_t *rc, *gc, *bc;
-  // BGR
-  const uint8_t *in_pixels = rgb.p;
-  uint8_t *yc = pic_in.img.plane[0];
-  uint8_t *uc = pic_in.img.plane[1];
-  uint8_t *vc = pic_in.img.plane[2];
   const unsigned int num_pixel = rgb.d0 * rgb.d1;
 
   if(!is_rgb) {
-#pragma omp parallel for schedule(guided, 256) num_threads(4)
-      for(int i = 0; i < num_pixel; ++i) {
-          const int pixel_index = i*3;
-          rgbToYuvVis(in_pixels[pixel_index+2], in_pixels[pixel_index+1], in_pixels[pixel_index], yc + i, uc + i, vc + i);
-      }
+      bgr2yuv(rgb.p, pic_in.img.plane[0], pic_in.img.plane[1], pic_in.img.plane[2], num_pixel);
   } else {
-#pragma omp parallel for schedule(guided, 256) num_threads(4)
-      for(int i = 0; i < num_pixel; ++i) {
-          const int pixel_index = i*3;
-          rgbToYuvVis(in_pixels[pixel_index], in_pixels[pixel_index+1], in_pixels[pixel_index+2], yc + i, uc + i, vc + i);
-      }
+      rgb2yuv(rgb.p, pic_in.img.plane[0], pic_in.img.plane[1], pic_in.img.plane[2], num_pixel);
   }
 
   clock_gettime(CLOCK_REALTIME, &end_csp);
@@ -227,5 +163,6 @@ VideoEncoder_x264_simple::VideoEncoder_x264_simple(const char*, uint, uint){
 }
 void VideoEncoder_x264_simple::addFrame(const byteA&){}
 void VideoEncoder_x264_simple::close(){ }
+void VideoEncoder_x264_simple::set_rgb(bool is_rgb){ }
 
 #endif // HAVE_LIBAV
