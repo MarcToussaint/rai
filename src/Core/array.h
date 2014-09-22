4e6fff27f9ab99cec6c1ca3994c85b19847fa8cf
Marc Toussaint <marc.toussaint@informatik.uni-stuttgart.de>
2014-09-22 12:24:53 +0200

    DANGEROUS: redid Item & KVG to ensure strict containment

--- src/Core/array.h
+++ src/Core/array.h
@@ -14,8 +14,7 @@
     
     You should have received a COPYING file of the GNU General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>
-    -----------------------------------------------------------------  */
-
+    ---------------------------------------------------------------  */
 
 /// @file
 /// @ingroup group_array
