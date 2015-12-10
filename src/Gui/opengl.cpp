0f0d9c57e9618198852705bfe6a26d548c210f12
Marc Toussaint <marc.toussaint@informatik.uni-stuttgart.de>
2015-12-10 14:48:56 +0100

    auto

--- src/Gui/opengl.cpp
+++ src/Gui/opengl.cpp
@@ -951,7 +951,7 @@ bool glUI::clickCallback(OpenGL& gl) { NICO }
 //
 
 OpenGL::OpenGL(const char* _title, int w, int h, int posx, int posy)
-  : s(NULL), title(_title), reportEvents(false), width(w), height(h), captureImg(false), captureDep(false), fboId(0), rboColor(0), rboDepth(0){
+  : s(NULL), title(_title), width(w), height(h), reportEvents(false), captureImg(false), captureDep(false), fboId(0), rboColor(0), rboDepth(0){
   //MLR_MSG("creating OpenGL=" <<this);
   Reshape(w,h);
   s=new sOpenGL(this); //this might call some callbacks (Reshape/Draw) already!
@@ -959,7 +959,7 @@ OpenGL::OpenGL(const char* _title, int w, int h, int posx, int posy)
 }
 
 OpenGL::OpenGL(void *container)
-  : s(NULL), reportEvents(false), width(0), height(0), captureImg(false), captureDep(false), fboId(0), rboColor(0), rboDepth(0){
+  : s(NULL), width(0), height(0), reportEvents(false), captureImg(false), captureDep(false), fboId(0), rboColor(0), rboDepth(0){
   s=new sOpenGL(this,container); //this might call some callbacks (Reshape/Draw) already!
   init();
 }
