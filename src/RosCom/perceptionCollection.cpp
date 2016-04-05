5a3bcefc45b4669b139d1ea4e990088ca41bb2d3
Matt Bernstein <matthew.bernstein@ipvs.uni-stuttgart.de>
2016-04-05 16:12:57 +0200

    Merge fixes

---
+++ src/RosCom/perceptionCollection.cpp
@@ -0,0 +1,98 @@
+#include <RosCom/roscom.h>
+#include <RosCom/perceptionCollection.h>
+
+Collector::Collector():
+    Module("Collector", 0){}
+
+void Collector::step()
+{
+  FilterObjects perceps;
+
+  int tabletop_rev = tabletop_clusters.readAccess();
+  int ar_rev = ar_pose_markers.readAccess();
+
+  if ( tabletop_rev > tabletop_revision )
+  {
+    tabletop_revision = tabletop_rev;
+    const visualization_msgs::MarkerArray msg = tabletop_clusters.get();
+    for(auto & marker : msg.markers){
+      perceps.append(conv_Marker2FilterObject( marker ));
+    }
+  }
+
+  if ( ar_rev > ar_pose_markers_revision)
+  {
+    ar_pose_markers_revision = ar_rev;
+    const ar::AlvarMarkers msg = ar_pose_markers.get();
+    for(auto & marker : msg.markers)
+    {
+      perceps.append(conv_Alvar2FilterObject( marker ) );
+    }
+  }
+
+  if (perceps.N > 0)
+  {
+    std::cout << "Collector update" << std::endl;
+    perceptual_inputs.set() = perceps;
+  }
+
+  ar_pose_markers.deAccess();
+  tabletop_clusters.deAccess();
+
+}
+
+
+FilterObject conv_Marker2FilterObject(const visualization_msgs::Marker& marker)
+{
+  arr pts = conv_points2arr(marker.points);
+  arr mean = sum(pts,0)/(double)pts.d0;
+  // Put it into our list
+
+  FilterObject new_object;
+  new_object.Cluster::mean = mean;
+  //std::cout << "Mean: " << mean(0) << ' ' << mean(1) << ' ' << mean(2) << std::endl;
+
+  new_object.Cluster::points = pts;
+  new_object.id = -1;
+  new_object.relevance = 1;
+  new_object.Cluster::frame_id = marker.header.frame_id;
+  new_object.type = FilterObject::FilterObjectType::cluster;
+  return new_object;
+}
+
+FilterObject conv_Alvar2FilterObject(const ar::AlvarMarker& marker)
+{
+  FilterObject new_object;
+  new_object.id = marker.id;
+  new_object.Alvar::frame_id = marker.header.frame_id;
+  new_object.relevance = 1;
+  new_object.type = FilterObject::FilterObjectType::alvar;
+
+  new_object.quaternion.x = marker.pose.pose.orientation.x;
+  new_object.quaternion.y = marker.pose.pose.orientation.y;
+  new_object.quaternion.z = marker.pose.pose.orientation.z;
+  new_object.quaternion.w = marker.pose.pose.orientation.w;
+
+#if 0
+  tf::TransformListener listener;
+  tf::StampedTransform baseTransform;
+  try{
+    listener.waitForTransform("/base_footprint", marker.header.frame_id, ros::Time(0), ros::Duration(1.0));
+    listener.lookupTransform("/base_footprint", marker.header.frame_id, ros::Time(0), baseTransform);
+  }
+  catch (tf::TransformException &ex) {
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+  }
+  tf::Vector3 position(marker.pose.pose.position.x, marker.pose.pose.position.y, marker.pose.pose.position.z);
+  position = baseTransform * position;
+
+  new_object.position = {position.x, position.y, position.z};
+
+#else
+  new_object.position = {marker.pose.pose.position.x, marker.pose.pose.position.y, marker.pose.pose.position.z};
+#endif
+
+  return new_object;
+}
+
---
+++ src/RosCom/subscribeAlvarMarkers.cpp
@@ -0,0 +1,88 @@
+#include "subscribeAlvarMarkers.h"
+
+#ifdef MLR_ROS
+
+#ifdef MLR_ROS_INDIGO
+  #include <ar_track_alvar_msgs/AlvarMarkers.h>
+  namespace ar = ar_track_alvar_msgs;
+#endif
+#if MLR_ROS_GROOVY
+  #include <ar_track_alvar/AlvarMarkers.h>
+  namespace ar = ar_track_alvar;
+#endif
+
+// ============================================================================
+// void ROSMODULE_markers::step() {
+//   modelWorld.writeAccess();
+//   AlvarMarkers markers_ = markers.get();
+//   syncMarkers(modelWorld.set()(), markers_);
+//   modelWorld.deAccess();
+// }
+
+// ============================================================================
+void setBody(ors::Body& body, const ar::AlvarMarker& marker) {
+  body.X = conv_pose2transformation(marker.pose.pose);
+}
+
+
+void syncMarkers(ors::KinematicWorld& world, const ar::AlvarMarkers& markers) {
+  bool createdNewMarkers = false;
+
+  // transform: torso_lift_link is the reference frame_id
+  ors::Body *torso = world.getBodyByName("torso_lift_link", false);
+  if(!torso) return; //TODO: make this more general!
+  ors::Transformation refFrame = torso->X;
+
+  for (const ar::AlvarMarker& marker : markers.markers) {
+    mlr::String marker_name = STRING("marker" << marker.id);
+
+    ors::Body *body = world.getBodyByName(marker_name);
+    if (not body) {
+      createdNewMarkers = true;
+      cout << marker_name << " does not exist yet; adding it..." << endl;
+      body = new ors::Body(world);
+      body->name = marker_name;
+      ors::Shape *shape = new ors::Shape(world, *body);
+      shape->name = marker_name;
+      shape->type = ors::markerST;
+      shape->size[0] = .3; shape->size[1] = .0; shape->size[2] = .0; shape->size[3] = .0;
+    }
+    ors::Vector Y_old;
+    ors::Vector Z_old;
+    Z_old = world.getShapeByName(marker_name)->X.rot.getZ();
+    Y_old = world.getShapeByName(marker_name)->X.rot.getY();
+    setBody(*body, marker);
+    ors::Transformation T;
+
+    T.setZero();
+    T.addRelativeRotationDeg(90.,0.,1.,0.);
+
+    body->X = refFrame * T * body->X;
+    body->X.addRelativeRotationDeg(-90.,0.,1.,0.);
+    body->X.addRelativeRotationDeg(-90.,1.,0.,0.);
+
+   // while (body->X.rot.getX().theta()  < M_PI / 2. || body->X.rot.getY().theta()  < M_PI / 2.){
+  //   body->X.addRelativeRotationDeg(-90.,0.,0.,1.);
+      //cout << "test" << endl;
+   // }
+    /*int i = 0;
+     while ( body->X.rot.getZ().angle(Z_old) > 1.3 || body->X.rot.getY().angle(Y_old) > 1.3){
+
+     body->X.addRelativeRotationDeg(-90.,1.,0.,0.);
+     //cout << body->X.rot.getX().angle(X_old) << "new" << i << endl;
+      if (i == 3)break;
+      i++;
+   }*/
+
+    world.getShapeByName(marker_name)->X = body->X;
+
+  }
+
+  if (createdNewMarkers) {
+    world.swiftDelete();
+  }
+}
+#else
+void setBody(ors::Body& body, const AlvarMarker& marker) {}
+void syncMarkers(ors::KinematicWorld& world, AlvarMarkers& markers) {}
+#endif
