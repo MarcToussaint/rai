646729bed104561140787896e4ad70a7119d4ab8
Johannes Kulick <kulick@hildensia.de>
2016-04-29 09:56:02 +0200

    RM is in the parent

--- src/RosCom/serviceRAP.cpp
+++ src/RosCom/serviceRAP.cpp
@@ -5,6 +5,8 @@
 
 #include <ros/ros.h>
 struct sServiceRAP{
+  ServiceRAP *p;
+  sServiceRAP(ServiceRAP *p) : p(p) {};
   ros::NodeHandle nh;
   ros::ServiceServer service;
   bool cb_service(mlr_srv::StringString::Request& _request, mlr_srv::StringString::Response& response);
@@ -14,7 +16,7 @@ ServiceRAP::ServiceRAP()
   : RM(NULL, "RM"), s(NULL){
   if(mlr::getParameter<bool>("useRos")){
     cout <<"*** Starting ROS Service RAP" <<endl;
-    s = new sServiceRAP;
+    s = new sServiceRAP(this);
     s->service = s->nh.advertiseService("/RAP/service", &sServiceRAP::cb_service, s);
   }
 }
@@ -26,25 +28,25 @@ ServiceRAP::~ServiceRAP(){
 bool sServiceRAP::cb_service(mlr_srv::StringString::Request& _request, mlr_srv::StringString::Response& response){
   mlr::String request = _request.str.c_str();
   if(request=="getState"){
-    mlr::String str = RM.get()->getState();
+    mlr::String str = p->RM.get()->getState();
     response.str = str.p;
     return true;
   }
   if(request=="getSymbols"){
     mlr::String str;
-    str <<RM.get()->getSymbols();
+    str <<p->RM.get()->getSymbols();
     response.str = str.p;
     return true;
   }
 
   cout <<"received new effect '" <<request <<"'" <<endl;
   if(!request.N) return false;
-  RM.writeAccess();
-  RM().applyEffect(request);
-  RM().fwdChainRules();
+  p->RM.writeAccess();
+  p->RM().applyEffect(request);
+  p->RM().fwdChainRules();
   mlr::String str;
-  RM().tmp->write(str," ");
-  RM.deAccess();
+  p->RM().tmp->write(str," ");
+  p->RM.deAccess();
   if(str.N)
     response.str = str.p;
   else
