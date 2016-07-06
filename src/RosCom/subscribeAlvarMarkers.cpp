659ec8937894facc7ea176d5de77d6985a3deba8
Peter Englert <peter.englert@ipvs.uni-stuttgart.de>
2016-07-06 15:45:27 +0200

    Merge branch 'peter'

--- src/RosCom/subscribeAlvarMarkers.cpp
+++ src/RosCom/subscribeAlvarMarkers.cpp
@@ -45,12 +45,34 @@ void syncMarkers(ors::KinematicWorld& world, const ar::AlvarMarkers& markers) {
       ors::Shape *shape = new ors::Shape(world, *body);
       shape->name = marker_name;
       shape->type = ors::markerST;
-      shape->size[0] = .01; shape->size[1] = .0; shape->size[2] = .0; shape->size[3] = .0;
+      shape->size[0] = .3; shape->size[1] = .0; shape->size[2] = .0; shape->size[3] = .0;
     }
+    ors::Vector Y_old;
+    ors::Vector Z_old;
+    Z_old = world.getShapeByName(marker_name)->X.rot.getZ();
+    Y_old = world.getShapeByName(marker_name)->X.rot.getY();
     setBody(*body, marker);
+    ors::Transformation T;
 
-    body->X.appendTransformation(refFrame);
-    body->X.addRelativeRotationDeg(90.,0.,1.,0.);
+    T.setZero();
+    T.addRelativeRotationDeg(90.,0.,1.,0.);
+
+    body->X = refFrame * T * body->X;
+    body->X.addRelativeRotationDeg(-90.,0.,1.,0.);
+    body->X.addRelativeRotationDeg(-90.,1.,0.,0.);
+
+   // while (body->X.rot.getX().theta()  < M_PI / 2. || body->X.rot.getY().theta()  < M_PI / 2.){
+  //   body->X.addRelativeRotationDeg(-90.,0.,0.,1.);
+      //cout << "test" << endl;
+   // }
+    /*int i = 0;
+     while ( body->X.rot.getZ().angle(Z_old) > 1.3 || body->X.rot.getY().angle(Y_old) > 1.3){
+
+     body->X.addRelativeRotationDeg(-90.,1.,0.,0.);
+     //cout << body->X.rot.getX().angle(X_old) << "new" << i << endl;
+      if (i == 3)break;
+      i++;
+   }*/
 
     world.getShapeByName(marker_name)->X = body->X;
 
