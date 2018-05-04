2cb479fe9fb65991a69efd3514d17b9928777b74
Marc Toussaint <marc.toussaint@informatik.uni-stuttgart.de>
2018-05-04 12:08:13 -0400

    flipSignOnScalarProduct within KOMO...

--- rai/KOMO/komo.cpp
+++ rai/KOMO/komo.cpp
@@ -1175,7 +1175,7 @@ void KOMO::playInPhysics(uint subSteps, bool display){
     }
     px.pullFromPhysx(configurations(k_order+t), vels);
   }
-//  for(uint i=0;i<vels.d0;i++) if(i<world.frames.N) cout <<world.frames(i)->name <<" v=" <<vels[i] <<endl;
+  //  for(uint i=0;i<vels.d0;i++) if(i<world.frames.N) cout <<world.frames(i)->name <<" v=" <<vels[i] <<endl;
 }
 
 void KOMO::reportProblem(std::ostream& os){
@@ -1785,9 +1785,14 @@ void KOMO::Conv_MotionProblem_KOMO_Problem::phi(arr& phi, arrA& J, arrA& H, uint
         if(absMax(y)>1e10) RAI_MSG("WARNING y=" <<y);
 
         //linear transform (target shift)
-        if(task->target.N==1) y -= task->target.elem(0);
-        else if(task->target.nd==1) y -= task->target;
-        else if(task->target.nd==2) y -= task->target[t];
+        arr target;
+        if(task->target.N==1) target = consts<double>(y.N, task->target.elem(0));
+        else if(task->target.nd==1) target = task->target;
+        else if(task->target.nd==2) target = task->target[t];
+        if(target.N){
+          if(task->map->flipTargetSignOnNegScalarProduct && scalarProduct(y, target)<-.0) target *= -1.;
+          y -= target;
+        }
         y *= sqrt(task->prec(t));
 
         //write into phi and J
@@ -1854,6 +1859,11 @@ void KOMO::Conv_MotionProblem_KOMO_Problem::phi(arr& phi, arrA& J, arrA& H, uint
 
 }
 
+rai::KinematicWorld& KOMO::getConfiguration(double phase){
+  uint s = k_order + (uint)(phase*double(stepsPerPhase));
+  return *configurations(s);
+}
+
 arr KOMO::getPath(const StringA &joints){
   arr X(T,joints.N);
   for(uint t=0;t<T;t++){
@@ -1878,9 +1888,4 @@ arr KOMO::getPath_times(){
     X(t) = configurations(t+k_order)->frames.first()->time;
   }
   return X;
-}
-
-arr KOMO::getPathPose(const StringA &joints, double phase){
-  uint s = k_order + (uint)(phase*double(stepsPerPhase));
-  return configurations(s)->getJointState(joints);
 }
--- rai/KOMO/komo.h
+++ rai/KOMO/komo.h
@@ -176,10 +176,10 @@ struct KOMO{
   void run();                     ///< run the optimization (using OptConstrained -- its parameters are read from the cfg file)
   void getPhysicsReference(uint subSteps=10, int display=0);
   void playInPhysics(uint subSteps=10, bool display=false);
+  rai::KinematicWorld& getConfiguration(double phase);
   arr getPath(const StringA& joints);
   arr getPath_frames(const uintA &frames);
   arr getPath_times();
-  arr getPathPose(const StringA& joints, double phase);
   void reportProblem(ostream &os=std::cout);
   Graph getReport(bool gnuplt=false, int reportFeatures=0, ostream& featuresOs=std::cout); ///< return a 'dictionary' summarizing the optimization results (optional: gnuplot task costs; output detailed cost features per time slice)
   void reportProxies(ostream& os=std::cout); ///< report the proxies (collisions) for each time slice
