f60cd1428b57ece82fca96f3eedce5b58042f26a
Marc Tuscher <marc.tuscher@gmail.com>
2019-12-18 15:03:42 +0100

    typecaster working -- cherry picked from tuscher

--- rai/ry/types.cpp
+++ rai/ry/types.cpp
@@ -118,4 +118,33 @@ arr vecvec2arr(const std::vector<std::vector<double> >& X) {
   return Y;
 }
 
+namespace pybind11 { namespace detail {
+    template <typename T> struct type_caster<rai::Array<T>> {
+    public:
+        PYBIND11_TYPE_CASTER(rai::Array<T>, _("rai::Array<T>"));
+
+        /**
+         * Conversion part 1 (Python->C++): convert numpy array to rai::Array<T>
+         */
+        bool load(handle src, bool) {
+
+            auto buf = pybind11::array_t<T>::ensure(src);
+            if ( !buf )
+                return false;
+            rai::Array<T> a = numpy2arr(buf);
+            value = a;
+            /* Ensure return code was OK (to avoid out-of-range errors etc) */
+            return !PyErr_Occurred();
+        }
+
+        /**
+         * Conversion part 2 (C++ -> Python): convert rai::Array<T> instance
+         * to numpy array.
+         */
+        static handle cast(rai::Array<T> src, return_value_policy /* policy */, handle /* parent */) {
+            pybind11::array ret(src.dim(), src.p);
+            return ret.release();
+        }
+    };
+}} // namespace pybind11::detail
 #endif
