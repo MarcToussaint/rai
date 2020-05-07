b87a9588f3229f9f05e65f170027ca7b774b4e7a
Marc Toussaint <toussaint@tu-berlin.de>
2020-05-07 14:48:01 +0200

    small fix in recursive graph read

--- rai/Core/graph.cpp
+++ rai/Core/graph.cpp
@@ -589,6 +589,7 @@ void Graph::copy(const Graph& G, bool appendInsteadOfClear, bool enforceCopySubg
 }
 
 void Graph::read(std::istream& is, bool parseInfo) {
+  uint Nbefore = N;
   if(parseInfo) getParseInfo(nullptr).beg=is.tellg();
   String namePrefix;
   StringA tags;
@@ -631,7 +632,11 @@ void Graph::read(std::istream& is, bool parseInfo) {
   DEBUG(checkConsistency());
 
   //-- merge all Mege keys
-  NodeL edits = getNodesWithTag("%Edit");
+  NodeL edits;// = getNodesWithTag("%Edit");
+  for(uint i=Nbefore; i<N; i++) {
+    Node* n=elem(i);
+    if(n->isGraph() && n->graph().findNode("%Edit")) edits.append(n);
+  }
   for(Node* ed:edits) {
 //    CHECK_EQ(ed->key.first(), "Edit", "an edit node needs Edit as first key");
     ed->graph().delNode(ed->graph().findNode("%Edit"));
