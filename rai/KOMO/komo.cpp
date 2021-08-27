85b10adf48e372d1c757e0d19a21843a1b24e615
Marc Toussaint <toussaint@tu-berlin.de>
2021-08-27 13:31:14 +0200

    minor

--- rai/KOMO/komo.cpp
+++ rai/KOMO/komo.cpp
@@ -1470,7 +1470,7 @@ StringA KOMO::getCollisionPairs(double belowMargin){
     if(!p.collision)((Proxy*)&p)->calc_coll();
     double d = p.collision->getDistance();
     if(d<belowMargin){
-      cout <<"KOMO collision pair: " <<p.a->name <<"--" <<p.b->name <<" : " <<p.d <<endl;
+//      cout <<"KOMO collision pair: " <<p.a->name <<"--" <<p.b->name <<" : " <<p.d <<endl;
       uint i=p.a->ID % nFrames;
       uint j=p.b->ID % nFrames;
       if(j<i){ int a=i; i=j; j=a; }
@@ -1486,7 +1486,7 @@ StringA KOMO::getCollisionPairs(double belowMargin){
     }
   }
   cols.reshape(-1, 2);
-  cout <<"KOMO collision pairs: " <<cols;
+//  cout <<"KOMO collision pairs: " <<cols;
   return cols;
 }
 
