8c3621a3cf2ff8235e6feb4ff87e40261a9fa393
lana <lana@levit.eu>
2021-09-07 15:12:38 +0200

    test for the lgp with the puzzle config

---
+++ test/LGP/pickAndPlacePuzzles/main.cpp
@@ -0,0 +1,115 @@
+#include <Kin/kin.h>
+#include <Gui/opengl.h>
+#include <Core/graph.h>
+#include <Kin/proxy.h>
+#include <Kin/viewer.h>
+
+#include <LGP/LGP_tree.h>
+#include <KOMO/komo.h>
+
+void generateProblem(rai::Configuration& C){
+  uint numObj = 4;
+  for(;;){
+    C.clear();
+    //C.addFile("../../../../rai-robotModels/pr2/pr2.g");
+    C.addFile("/home/lana/git/p30branchraimarc/puzzle-scenes/scenes-30a/0k-2d-room-ego-obj-panda.g");
+    //C.selectJointsByAtt({"ego"});
+    //C.selectJointsByAtt({"base","armL","armR"});
+    ///C.pruneInactiveJoints();
+    C.optimizeTree();
+    //C["pr2L"]->ats->newNode<rai::Graph>({"logical"}, {}, {{"gripper", true}});
+    //C["pr2R"]->ats->newNode<rai::Graph>({"logical"}, {}, {{"gripper", true}});
+    //C["worldTranslationRotation"]->joint->H = 1e-0;   
+    ///C.addFile("../../../../rai-robotModels/objects/tables.g");
+    for(uint i=0;i<numObj;i++){
+      rai::Frame *f = C.addFrame(STRING("obj"<<i), "floor", "type:ssBox size:[.1 .1 .2 .02] color:[1. 0. 0.], contact, logical={ object }, joint:rigid" );
+      f->setRelativePosition({rnd.uni(-.3, .3), rnd.uni(-1.,1.), .15});
+      f->setRelativeQuaternion(rai::Quaternion(0).addZ(rnd.uni(-RAI_PI,RAI_PI)).getArr4d());
+    }
+    C.stepSwift(); 
+    arr y, J;
+    C.kinematicsPenetration(y, J); 
+//    cout <<"collision costs of config: " <<y.scalar() <<endl;
+//    K.reportProxies(cout, .1, true);
+//    K.watch();
+    if(y.scalar()==0.) break; 
+  }
+
+  C.proxies.clear();
+
+  rai::Frame *f = C.addFrame("tray", "floor", "type:ssBox size:[.15 .15 .04 .02] color:[0. 1. 0.], logical={ table }" );
+  f->setRelativePosition({0.,0.,.07});
+
+  C.addFrame("", "tray", "type:ssBox size:[.27 .27 .04 .02] color:[0. 1. 0.]" );
+
+}
+
+void solve(){
+  rai::Configuration C;
+  generateProblem(C);
+  //  K.addFile("model2.g");
+  C.selectJointsByAtt({"ego"});
+  C.optimizeTree();
+
+  rai::LGP_Tree lgp(C, "fol-pnp-switch.g");
+  lgp.fol.addTerminalRule("(on tray obj0) (on tray obj1) (on tray obj2)"); 
+  lgp.displayBound = rai::BD_seqPath;
+  //lgp.verbose=2;
+
+  lgp.fol.writePDDLfiles("z");
+
+  lgp.run();
+
+  for(auto* s:lgp.solutions.set()()){
+    cout <<"SOLUTION:\n";
+    s->write(cout);
+    cout <<endl;
+  }
+
+  rai::wait();
+  // if(lgp.verbose>1){
+  //   rai::wait();
+  //   lgp.renderToVideo();
+  // }
+}
+
+void playIt(){
+  rai::Configuration C;
+  generateProblem(C);
+  rai::LGP_Tree lgp(C, "fol-pnp-switch.g");
+  lgp.player();
+}
+
+void testBounds(){
+  rai::Configuration C;
+  generateProblem(C);
+//  K.addFile("model2.g");
+
+  rai::LGP_Tree lgp(C, "fol-pnp-switch.g");
+
+//  lgp.inspectSequence("(pick pr2R obj0) (place pr2R obj0 tray)");
+//  lgp.inspectSequence("(pick pr2R obj0) (pick pr2L obj1) (place pr2R obj0 tray) (place pr2L obj1 tray) (pick pr2L obj2) (place pr2L obj2 tray)");
+  lgp.inspectSequence("(pick ego obj0) (place ego obj0 tray) (pick ego obj1) (place ego obj1 tray) (pick ego obj2) (place ego obj2 tray)");
+  return;
+
+  rai::LGP_Node* node = lgp.walkToNode("(pick pr2R obj0) (pick pr2L obj3) (place pr2R obj0 tray) (place pr2L obj3 tray)");
+  rai::BoundType bound = rai::BD_path;
+  node->optBound(bound, true, 2);
+  rai::ConfigurationViewer V;
+  node->displayBound(V, bound);
+//  V.setPath(node->problem(bound).komo->getPath_X(), "", true);
+//  for(uint i=0;i<2;i++) V.playVideo(true, 3.);
+}
+
+int MAIN(int argc,char **argv){
+  rai::initCmdLine(argc, argv);
+//  rnd.clockSeed();
+
+  //solve();
+
+  playIt();
+
+  testBounds();
+
+  return 0;
+}
